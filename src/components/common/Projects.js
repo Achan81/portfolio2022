fb3fc0c3044f97258b2786b7323d43d55542f300
achan81 <achan81@icloud.com>
2022-02-22 22:29:24 +0000

    project grammar updated

--- src/components/common/Projects.js
+++ src/components/common/Projects.js
@@ -41,12 +41,8 @@ function Projects() {
         <Collapse in={open1}>
           <div id="collapse-text1" className="container content fw-lighter">
             <p className="col hidden-text bgClip">A grid based shoot em up game built in JavaScript, HTML & CSS.
-              Here I was able to apply everything learnt in the first few weeks and present it in the form of a single player game. 
-              Although a classic arcade game, I focused on making sure the player controls and gameplay runs smoothly. 
-              I also added up down controls feature to allow the user experience to be more advanced. 
-              The styling is a mix of retro and contemporary, with a space themed background. 
-              The game is not mobile friendly and sound files were corrupted when deploying. 
-              I would very much like to revisit and improve the game further.</p>
+            Here, I was able to apply everything learnt in the first few weeks and present it in the form of a single player game. Although a classic arcade game, I focused on making sure the player controls and gameplay runs smoothly. I also added up down controls feature to allow the user experience to be more advanced. The styling is a mix of retro and contemporary, with a space themed background. The game is not mobile friendly and sound files were corrupted when deploying. I would very much like to revisit and improve the game further.
+            </p>
 
             <div className="container">
               <div className="d-flex flex-row justify-content-center mt-n20">
@@ -72,11 +68,7 @@ function Projects() {
         </a>
         <Collapse in={open2}>
           <div id="collapse-text2" className="content fw-lighter">
-            <p className="hidden-text bgClip ">As a pair, we built a React.js app which used an external Superhero-API (Multiple universes superheroes open-source REST API). 
-        The project allowed me to learn to pair-code, project plan, and call data from existing API’s. 
-        Bulma CSS Framework used for the first time. 
-        This project helped me to understand my own strengths and weaknesses at this stage of the course. 
-        The app functions as a database for searching and filtering from a variety of heroes/villains from various publishers to see and compare stats.</p>
+            <p className="hidden-text bgClip ">As a pair, we built a React.js app which used an external Superhero-API (Multiple universes superheroes open-source REST API). The project allowed me to learn to pair-code, project plan, and call data from existing API’s. Bulma CSS Framework was used for the first time. This project helped me to understand my own strengths and weaknesses at this stage of the course. The app functions as a database for searching and filtering from a variety of heroes/villains from various publishers to see and compare stats.</p>
             
             <div className="container">
               <div className="d-flex flex-row justify-content-center mt-n20">
@@ -103,11 +95,8 @@ function Projects() {
         </a>
         <Collapse in={open3}>
           <div id="collapse-text3" className="content fw-lighter">
-            <p className="hidden-text bgClip ">As a pair, we built a React.js app which used an external Superhero-API (Multiple universes superheroes open-source REST API). 
-            Full-Stack MERN app with CRUD functionality - MongoDB, Express.js, React.js & Node.js. 
-          As a group, we created an app which allowed users to share travel memories in the form of photo uploads and geo mapping locations that linked with memories based on field input. 
-          We all worked Full-Stack, while my specific focus was on building the front end structure, CSS and designing to be mobile ready. 
-          CSS framework used on this project was Bootstrap and part React Bootstrap.</p>
+            <p className="hidden-text bgClip ">Full-Stack MERN app with CRUD functionality - MongoDB, Express.js, React.js & Node.js. As a group, we created an app which allowed users to share travel memories in the form of photo uploads and geo mapping locations that linked with memories based on field input. We all worked Full-Stack, while my specific focus was on building the front end structure, CSS and designing to be mobile ready. The CSS framework used on this project was Bootstrap 5.0 and a little bit of React Bootstrap.
+            </p>
             <div className="container">
               <div className="d-flex flex-row justify-content-center mt-n20">
                 <a href="https://placebookapp.netlify.app/" rel="noopener noreferrer" target="_blank">
@@ -132,12 +121,7 @@ function Projects() {
         </a>
         <Collapse in={open4}>
           <div id="collapse-text4" className="content fw-lighter">
-            <p className="hidden-text bgClip bgClip">Full-Stack MERN app with CRUD functionality using Python, Django API and SQL database, with React.js & TailwindCSS. 
-          As a group, we made a clone app of the Dogs Trust website (renamed as Pawhub). 
-          Aside from styling the site closely to its original, its key features mirrored things we learnt over the course. 
-          Such as secure registration & log in, presenting dog index and search functionality, show pages. 
-          Collectively we completed the Back End and seeded a database together. On the Front End we each had pages to build, my main contributions were the questionnaire forms and donation page. 
-          CSS framework used on this project was Tailwind.</p>
+            <p className="hidden-text bgClip bgClip">Full-Stack MERN app with CRUD functionality using Python, Django API and SQL database, with React.js & TailwindCSS. As a group, we made a clone app of the Dogs Trust website (renamed as Pawhub). Aside from styling the site closely to its original, its key features mirrored things we learnt over the course, such as secure registration & log in, presenting dog index and search functionality, show pages. Collectively, we completed the Back End and seeded a database together. On the Front End, we each had pages to build, my main contributions were the questionnaire forms and the donation page. CSS framework used on this project was Tailwind 3.0.</p>
             <div className="container">
               <div className="d-flex flex-row justify-content-center mt-n20">
                 <a href="https://pawhubz.netlify.app/" rel="noopener noreferrer" target="_blank">
