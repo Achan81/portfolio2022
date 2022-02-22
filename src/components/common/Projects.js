8c213508aa7e881819f6695cd4e9124b48b2bb9c
achan81 <achan81@icloud.com>
2022-02-22 15:12:41 +0000

    expand projects functionality improved and images added

--- src/components/common/Projects.js
+++ src/components/common/Projects.js
@@ -7,8 +7,12 @@ import { Button, Collapse } from 'react-bootstrap'
 
 function Projects() {
   AOS.init()
-  const [open, setOpen] = React.useState(false)
   
+  const [open1, setOpen1 ] = React.useState(false)
+  const [open2, setOpen2 ] = React.useState(false)
+  const [open3, setOpen3 ] = React.useState(false)
+  const [open4, setOpen4 ] = React.useState(false)
+
   return (
 
     <section className="mt-50 blurry-text opacity-75" id="projects">
@@ -18,39 +22,38 @@ function Projects() {
         <div className="bgClip fs-6 mb-0 fw-bold">General Assembly - Software Engineering Immersive Course:</div>
         <div className="bgClip fs-6 mb-0 mt-n20">London UK (Remote) - NOV 2021 - FEB 2022</div>
         <div className="bgClip fw-lighter fs-6 mb-0 fst-italic mt-n20">
-  A 12-week immersive course learning fundamentals of web programming from front end to back end (focussed on JavaScript, React.js, HTML and CSS). 
-  The course consisted of daily stand-ups, online-lessons, labs, pair-coding, group & individual projects where we learnt how to build Full-Stack applications.
+          A 12-week immersive course learning fundamentals of web programming from front end to back end (focussed on JavaScript, React.js, HTML and CSS). 
+          The course consisted of daily stand-ups, online-lessons, labs, pair-coding, group & individual projects where we learnt how to build Full-Stack applications.
         </div>
         <hr className="bg-dark border-5 border-top border-dark"></hr>
-
 
         {/* PROJECT 1 * * * * * */}
         <Button
           className="bgClip project-title bg-transparent text-uppercase text-dark border-none btn-outline-dark fw-bold"
-          onClick={() => setOpen(!open)}
-          aria-controls="example-collapse-text1"
-          aria-expanded={open}
-          href="#collapse-text1"
-          data-bs-toggle="collapse"
+          onClick={() => setOpen1(!open1)}
+          aria-controls="collapse-text1"
+          aria-expanded={open1}
         > space invaders | solo project | 7 days
         </Button>
-        <Collapse in={open}>
-          <div id="example-collapse-text1" className="content fw-lighter">
-            <p className='hidden-text bgClip'>A grid based shoot em up game built in JavaScript, HTML & CSS.
+        <Collapse in={open1}>
+          <div id="collapse-text1" className="container content fw-lighter">
+            <p className="col hidden-text bgClip">A grid based shoot em up game built in JavaScript, HTML & CSS.
               Here I was able to apply everything learnt in the first few weeks and present it in the form of a single player game. 
               Although a classic arcade game, I focused on making sure the player controls and gameplay runs smoothly. 
               I also added up down controls feature to allow the user experience to be more advanced. 
               The styling is a mix of retro and contemporary, with a space themed background. 
               The game is not mobile friendly and sound files were corrupted when deploying. 
               I would very much like to revisit and improve the game further.</p>
+
             <div className="container">
-              <div className="d-flex flex-row justify-content-center">
+              <div className="d-flex flex-row justify-content-center mt-n20">
                 <a href="https://achan81.github.io/sei-project-01/" rel="noopener noreferrer" target="_blank">
                   <img alt="project1" src="https://i.imgur.com/jYp35wH.png" className="icon project1-image col"/></a>
                 <a href="https://github.com/Achan81/sei-project-01/blob/main/readme.md" rel="noopener noreferrer" target="_blank">
                   <img alt="readme1" src="https://i.imgur.com/yWic7vC.png" className="icon readme1-image col"/></a>
               </div>
             </div>
+            <img src= "https://i.imgur.com/9F1ukTo.png" className="col-6 rounded border border-1 border-dark"/>
           </div>
         </Collapse>        
         <hr className="bg-dark border-5 border-top border-dark"></hr>
@@ -58,22 +61,21 @@ function Projects() {
         {/* PROJECT 2 * * * * * */}
         <Button
           className="bgClip project-title bg-transparent text-uppercase text-dark border-none btn-outline-dark fw-bold"
-          onClick={() => setOpen(!open)}
-          aria-controls="example-collapse-text2"
-          aria-expanded={open}
-          href="#collapse-text2"
-          data-bs-toggle="collapse"
+          onClick={() => setOpen2(!open2)}
+          aria-controls="collapse-text2"
+          aria-expanded={open2}
         > heros & villains db | pair project | 48 hours hackathon
         </Button>
-        <Collapse in={open}>
-          <div id="example-collapse-text2" className="content fw-lighter">
-            <p className='hidden-text bgClip'>As a pair, we built a React.js app which used an external Superhero-API (Multiple universes superheroes open-source REST API). 
+        <Collapse in={open2}>
+          <div id="collapse-text2" className="content fw-lighter">
+            <p className="hidden-text bgClip">As a pair, we built a React.js app which used an external Superhero-API (Multiple universes superheroes open-source REST API). 
         The project allowed me to learn to pair-code, project plan, and call data from existing API’s. 
         Bulma CSS Framework used for the first time. 
         This project helped me to understand my own strengths and weaknesses at this stage of the course. 
         The app functions as a database for searching and filtering from a variety of heroes/villains from various publishers to see and compare stats.</p>
+            
             <div className="container">
-              <div className="d-flex flex-row justify-content-center">
+              <div className="d-flex flex-row justify-content-center mt-n20">
                 <a href="https://havdb.netlify.app/" rel="noopener noreferrer" target="_blank">
                   <img alt="project2" src="https://i.imgur.com/XlwKBFw.png" className="icon project2-image col"/></a>
                   
@@ -81,6 +83,7 @@ function Projects() {
                   <img alt="readme2" src="https://i.imgur.com/yWic7vC.png" className="icon readme2-image col"/></a>
               </div>
             </div>
+            <img src= "https://i.imgur.com/TRddwqX.png" className="col-6 rounded border border-1 border-dark"/>
           </div>
         </Collapse>        
         <hr className="bg-dark border-5 border-top border-dark"></hr>
@@ -88,28 +91,27 @@ function Projects() {
         {/* PROJECT 3 * * * * * */}
         <Button
           className="bgClip project-title bg-transparent text-uppercase text-dark border-none btn-outline-dark fw-bold"
-          onClick={() => setOpen(!open)}
-          aria-controls="example-collapse-text3"
-          aria-expanded={open}
-          href="#collapse-text3"
-          data-bs-toggle="collapse"
+          onClick={() => setOpen3(!open3)}
+          aria-controls="collapse-text3"
+          aria-expanded={open3}
         > placebook | group project | 7 days
         </Button>
-        <Collapse in={open}>
-          <div id="example-collapse-text3" className="content fw-lighter">
-            <p className='hidden-text bgClip'>As a pair, we built a React.js app which used an external Superhero-API (Multiple universes superheroes open-source REST API). 
+        <Collapse in={open3}>
+          <div id="collapse-text3" className="content fw-lighter">
+            <p className="hidden-text bgClip">As a pair, we built a React.js app which used an external Superhero-API (Multiple universes superheroes open-source REST API). 
             Full-Stack MERN app with CRUD functionality - MongoDB, Express.js, React.js & Node.js. 
           As a group, we created an app which allowed users to share travel memories in the form of photo uploads and geo mapping locations that linked with memories based on field input. 
           We all worked Full-Stack, while my specific focus was on building the front end structure, CSS and designing to be mobile ready. 
           CSS framework used on this project was Bootstrap and part React Bootstrap.</p>
             <div className="container">
-              <div className="d-flex flex-row justify-content-center">
+              <div className="d-flex flex-row justify-content-center mt-n20">
                 <a href="https://placebookapp.netlify.app/" rel="noopener noreferrer" target="_blank">
                   <img alt="project3" src="https://i.imgur.com/XUaI8kk.png" className="icon project3-image col"/></a>
                 <a href="https://github.com/Achan81/sei-project-three-frontend/blob/main/README.md" rel="noopener noreferrer" target="_blank">
                   <img alt="readme3" src="https://i.imgur.com/yWic7vC.png" className="icon readme3-image col"/></a>
               </div>
             </div>
+            <img src= "https://i.imgur.com/uj97fLV.png" className="col-6 rounded border border-1 border-dark"/>
           </div>
         </Collapse>        
         <hr className="bg-dark border-5 border-top border-dark"></hr>
@@ -117,29 +119,28 @@ function Projects() {
         {/* PROJECT 4 * * * * * */}
         <Button
           className="bgClip project-title bg-transparent text-uppercase text-dark border-none btn-outline-dark fw-bold"
-          onClick={() => setOpen(!open)}
-          aria-controls="example-collapse-text4"
-          aria-expanded={open}
-          href="#collapse-text4"
-          data-bs-toggle="collapse"
+          onClick={() => setOpen4(!open4)}
+          aria-controls="collapse-text4"
+          aria-expanded={open4}
         > pawhub | group project | 7 days
         </Button>
-        <Collapse in={open}>
-          <div id="example-collapse-text4" className="content fw-lighter">
-            <p className='hidden-text bgClip'>Full-Stack MERN app with CRUD functionality using Python, Django API and SQL database, with React.js & TailwindCSS. 
+        <Collapse in={open4}>
+          <div id="collapse-text4" className="content fw-lighter">
+            <p className="hidden-text bgClip">Full-Stack MERN app with CRUD functionality using Python, Django API and SQL database, with React.js & TailwindCSS. 
           As a group, we made a clone app of the Dogs Trust website (renamed as Pawhub). 
           Aside from styling the site closely to its original, its key features mirrored things we learnt over the course. 
           Such as secure registration & log in, presenting dog index and search functionality, show pages. 
           Collectively we completed the Back End and seeded a database together. On the Front End we each had pages to build, my main contributions were the questionnaire forms and donation page. 
           CSS framework used on this project was Tailwind.</p>
             <div className="container">
-              <div className="d-flex flex-row justify-content-center">
+              <div className="d-flex flex-row justify-content-center mt-n20">
                 <a href="https://pawhubz.netlify.app/" rel="noopener noreferrer" target="_blank">
                   <img alt="project4" src="https://i.imgur.com/xB9oxFs.png" className="icon project4-image col"/></a>
                 <a href="https://github.com/Achan81/sei-project-four-fe/blob/main/README.mdpaw" rel="noopener noreferrer" target="_blank">
                   <img alt="readme4" src="https://i.imgur.com/yWic7vC.png" className="icon readme4-image col"/></a>
               </div>
             </div>
+            <img src= "https://i.imgur.com/IbSn5i5.png" className="col-6 rounded border border-1 border-dark"/>
           </div>
         </Collapse>        
         <hr className="bg-dark border-5 border-top border-dark"></hr>
